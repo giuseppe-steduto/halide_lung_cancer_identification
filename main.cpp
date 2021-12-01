18637ce5df3f9bed86297b1ae329ad264708e7bd
Giuseppe Steduto <peppesteduto@gmail.com>
2021-12-01 23:10:34 +0100

    Checkpoint color channel (not working)

--- main.cpp
+++ main.cpp
@@ -1,35 +1,16 @@
 // The only Halide header file you need is Halide.h. It includes all of Halide.
 #include "Halide.h"
+#include "tools/halide_image_io.h"
 //Include library to open/save images
 #include <dcmtk/dcmimgle/dcmimage.h>
+#include <dcmtk/dcmdata/dcdatset.h>
+#include <dcmtk/dcmdata/dcfilefo.h>
+#include <dcmtk/dcmdata/dcitem.h>
 
 using namespace Halide;
 using std::cout;
-
-//Gamma correction
-void gamma(const uint8_t* src, uint8_t* dst, int height, int width, float gamma, bool gpu) {
-    static Func f("gamma_correction");
-    static Buffer<uint8_t> inp((uint8_t*) src, 3, width, height), out(dst, 3, width, height);
-
-    if (!f.defined()) {
-        Var x("x"), y("y"), c("c"), xo("xo"), xi("xi"), yo("yo"), yi("yi");
-        f(c, x, y) = cast<uint8_t>(255 * pow(inp(c, x, y) * 1.0f / 255, gamma));
-        Target t = Halide::get_host_target();
-        if (gpu) {
-            t.set_feature(Halide::Target::OpenCL);
-            f.bound(x, 0, width).bound(y, 0, height).bound(c, 0, 3)
-                    .split(x, xo, xi, 16).split(y, yo, yi, 16).reorder(xi, yi, c, xo, yo)
-                    .gpu_blocks(c, xo, yo).gpu_threads(xi, yi);
-        } else {
-            f.set_estimate(x, 0, width).set_estimate(y, 0, height).set_estimate(c, 0, 3);
-            Pipeline(f).auto_schedule(t);
-        }
-        f.compile_jit(t);
-    }
-    inp.set_host_dirty();
-    f.realize(out);
-    out.copy_to_host();
-}
+using std::cerr;
+using std::endl;
 
 Buffer<uint8_t> read_dicom_image(const char* filename) {
     DicomImage *image = new DicomImage(filename);
@@ -44,27 +25,72 @@ Buffer<uint8_t> read_dicom_image(const char* filename) {
             throw std::runtime_error("There is no data in the image!");
         }
     } else {
-        std::cerr << "Error: cannot load DICOM image (" << DicomImage::getString(image->getStatus()) << ")"
-                  << std::endl;
+        cerr << "Error: cannot load DICOM image (" << DicomImage::getString(image->getStatus()) << ")"
+                  << endl;
         delete image;
         throw std::runtime_error("Cannot load DICOM image!");
     }
 }
 
+Expr sobel (Func in)
+{
+    Func h("sobel_horiz"), v("sobel_vert");
+    Func sob("sobel");
+    Var x("x"), y("y"), c("c");
+    h(x, y, c) = in(x+1, y-1, c) + 2*in(x+1, y, c) +
+             in(x+1, y+1, c) - in(x-1, y-1, c) -
+             2 * in(x-1, y, c) - in(x-1,y+1, c);
+    v(x, y, c) = -in(x-1,y+1, c) - 2*in(x,y+1, c) -
+             in(x+1,y+1, c) + in(x-1,y-1, c) +
+             2 * in( x,y-1, c) + in( x+1,y-1, c);
+    sob(x, y, c) = (abs(h(x,y,c)) +
+                abs(v(x,y,c))) / 4;
+    return sob(x, y, c);
+}
+
+Expr binarize (Func in) {
+    Func bin("binarize");
+    Var x("x"), y("y"), c("c");
+    int threshold = 128;
+    Expr tmp = in(x, y, c) - threshold;
+    if (is_positive_const(tmp))
+        return cast<uint8_t>(1);
+    else
+        return cast<uint8_t>(0);
+}
+
 int main(int argc, char **argv) {
-    Buffer<uint8_t> image;
+    Buffer<uint8_t> image, complete;
     float gamma_exponent = 1.5; //Gamma correction constant
-    Func gamma;
+    Func gamma("gamma"), sobel_ed("sobel_edge_detecteor"), binarized("binarized_image");
     Var x("x"), y("y"), c("c"), xo("xo"), xi("xi"), yo("yo"), yi("yi");
     try {
         image = read_dicom_image("/home/giuseppe/Desktop/PoliMi/NECSTCamp/LungCancerIdentification/1-001.dcm");
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        cerr << e.what() << endl;
     }
-    gamma(c, x, y) = cast<uint8_t>(255 * pow(image(c, x, y) * 1.0f / 255, gamma_exponent));
-    gamma.realize();
+    gamma(x, y, c) = print(cast<uint8_t>(255 * pow(image(x, y, c) * 1.0f / 255, gamma_exponent)));
+    sobel_ed(x, y, c) = sobel(gamma);
+    binarized(x, y, c) = binarize(sobel_ed);
+
+    complete = binarized.realize({image.width(), image.height(), image.channels()});
+
+    //TODO other stuff
+
+    //Create output file
+    complete.copy_to_host();
 
 
+    DcmFileFormat output_file;
+    DcmDataset *dataset = output_file.getDataset();
+    //TODO insert metadata
+    DcmTag dcm_tag_pixeldata = DcmTag();
+    Tools::convert_and_save_image(complete, "test.jpg");
+    /*Uint8 image_buffer = save<uint8_t>(complete);
+    dataset->putAndInsertUint8Array(dcm_tag_pixeldata, &complete, complete.size_in_bytes());
+    OFCondition status = output_file.saveFile("test.dcm", EXS_LittleEndianExplicit);
+    if (status.bad())
+        cerr << "Error: cannot write DICOM file (" << status.text() << ")" << endl;*/
 
     return 0;
 }
